7986a38e5cb786c0ed5ee875d4a6ae2ef08bfa1b
Adnan <adnan.grbesic@edu.fit.ba>
2025-03-26 21:50:33 +0100

    test-commit

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -15,7 +15,7 @@ bool FlutterWindow::OnCreate() {
   }
 
   RECT frame = GetClientArea();
-
+  //git-test
   // The size here must match the window dimensions to avoid unnecessary surface
   // creation / destruction in the startup path.
   flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
