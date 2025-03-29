d76ff5366e73e7573381b3c3a609a49cffe0c0db
Adnan <adnan.grbesic@edu.fit.ba>
2025-03-29 21:02:34 +0100

    04-full-screen-support
    Support for the App to run in full screen via the Window Manager plug-in

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:arcade_os/models/game.dart';
 import 'package:arcade_os/services/game_service.dart';
 import 'package:flutter/material.dart';
 import 'package:arcade_os/pages/home_page.dart';
+import 'package:flutter/services.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
   databaseFactory = databaseFactoryFfi;
-
   List<Game> gamesFromDirectory = await GameService.loadGamesFromDirectory();
-
+  //await windowManager.setFullScreen(true);
   await GameService.saveGamesToDatabase(gamesFromDirectory);
 
   runApp(ArcadeOSApp());
