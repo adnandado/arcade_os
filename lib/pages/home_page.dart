aa4bf2bc555a0d5c87e90e4aa13f65daf04e6b04
Adnan <adnan.grbesic@edu.fit.ba>
2025-04-07 17:52:07 +0200

    06-scroll-and-zoom
    Added better scrolling in between games and zoom animation for the background

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,53 +1,76 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:arcade_os/config/config.dart';
 import 'package:flutter/material.dart';
 import 'package:arcade_os/services/game_service.dart';
 import 'package:arcade_os/models/game.dart';
-import 'dart:io';
 import 'package:arcade_os/widgets/game_tile.dart';
 import 'package:watcher/watcher.dart';
-import 'package:flutter/services.dart'; 
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   List<Game> recentlyPlayed = [];
   List<Game> popularGames = [];
   List<Game> recentlyAdded = [];
   DirectoryWatcher? _watcher;
+  final ScrollController _verticalScrollController = ScrollController();
+  final List<ScrollController> _horizontalScrollControllers = [
+    ScrollController(),
+    ScrollController(),
+    ScrollController(),
+  ];
 
   int selectedRowIndex = 0;
   int selectedGameIndex = 0;
   FocusNode _focusNode = FocusNode();
+  late AnimationController _zoomController;
+  late Animation<double> _zoomAnimation;
+
   @override
   void initState() {
     super.initState();
     _loadGames();
     _startWatchingFolder();
+
+    _zoomController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _zoomAnimation = Tween<double>(begin: 1.0, end: 3.0).animate(
+      CurvedAnimation(parent: _zoomController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _zoomController.dispose();
+    _focusNode.dispose();
+    super.dispose();
   }
 
   Future<void> _loadGames() async {
-    List<Game> games = await Game.getGamesFromDirectory(
-      Config.gamesDirectory,
-    );
-
+    List<Game> games = await Game.getGamesFromDirectory(Config.gamesDirectory);
     List<Game> gamesFromDB = await GameService.loadGamesFromDB();
 
     for (var game in games) {
       var dbGame = gamesFromDB.firstWhere(
         (dbGame) => dbGame.name == game.name,
-        orElse: () => Game(
-          name: game.name,
-          executablePath: game.executablePath,
-          coverImagePath: game.coverImagePath,
-          playCount: 0,
-          dateAdded: DateTime.now().toIso8601String(),
-          lastPlayed: null,
-        ),
+        orElse:
+            () => Game(
+              name: game.name,
+              executablePath: game.executablePath,
+              coverImagePath: game.coverImagePath,
+              playCount: 0,
+              dateAdded: DateTime.now().toIso8601String(),
+              lastPlayed: null,
+            ),
       );
 
       if (dbGame != null) {
@@ -77,27 +100,32 @@ class _HomePageState extends State<HomePage> {
         if (selectedRowIndex > 0) {
           setState(() {
             selectedRowIndex--;
-            selectedGameIndex = 0; 
+            selectedGameIndex = 0;
           });
+          scrollToSelection();
         }
       } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
         if (selectedRowIndex < 2) {
           setState(() {
             selectedRowIndex++;
-            selectedGameIndex = 0; 
+            selectedGameIndex = 0;
           });
+          scrollToSelection();
         }
       } else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
         if (selectedGameIndex > 0) {
           setState(() {
             selectedGameIndex--;
           });
+          scrollToSelection();
         }
       } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
-        if (selectedGameIndex < _getGameListByRowIndex(selectedRowIndex).length - 1) {
+        if (selectedGameIndex <
+            _getGameListByRowIndex(selectedRowIndex).length - 1) {
           setState(() {
             selectedGameIndex++;
           });
+          scrollToSelection();
         }
       } else if (event.logicalKey == LogicalKeyboardKey.enter) {
         _startGame(_getGameListByRowIndex(selectedRowIndex)[selectedGameIndex]);
@@ -107,10 +135,14 @@ class _HomePageState extends State<HomePage> {
 
   List<Game> _getGameListByRowIndex(int index) {
     switch (index) {
-      case 0: return recentlyPlayed;
-      case 1: return popularGames;
-      case 2: return recentlyAdded;
-      default: return [];
+      case 0:
+        return recentlyPlayed;
+      case 1:
+        return popularGames;
+      case 2:
+        return recentlyAdded;
+      default:
+        return [];
     }
   }
 
@@ -132,89 +164,135 @@ class _HomePageState extends State<HomePage> {
         });
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/background.png'),
-          fit: BoxFit.cover,
-          colorFilter: ColorFilter.mode(
-            Colors.black.withOpacity(0.4),
-            BlendMode.darken,
-          ),
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Space out content
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
+  void scrollToSelection() {
+    final double rowHeight = 350 + 60;
+    final double offsetY = selectedRowIndex * rowHeight;
+    _verticalScrollController.animateTo(
+      offsetY,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+
+    final double tileWidth = 200 + 16;
+    final double offsetX = selectedGameIndex * tileWidth;
+
+    final controller = _horizontalScrollControllers[selectedRowIndex];
+    controller.animateTo(
+      offsetX,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void _startWatchingFolder() {
+    final path = Config.gamesDirectory;
+    _watcher = DirectoryWatcher(path);
+    _watcher!.events.listen((event) {
+      print('Detected change in folder: ${event.type} ${event.path}');
+      _loadGames();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RawKeyboardListener(
+        focusNode: _focusNode,
+        onKey: _handleKeyEvent,
+        child: AnimatedBuilder(
+          animation: _zoomController,
+          builder: (context, child) {
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.cover,
+                  scale: _zoomAnimation.value,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.4),
+                    BlendMode.darken,
+                  ),
+                ),
+              ),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _buildGameRow("Most Recently Played", recentlyPlayed, 0),
-                  _buildGameRow("Popular Games", popularGames, 1),
-                  _buildGameRow("Recently Added", recentlyAdded, 2),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      controller: _verticalScrollController,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          _buildGameRow(
+                            "Most Recently Played",
+                            recentlyPlayed,
+                            0,
+                          ),
+                          _buildGameRow("Popular Games", popularGames, 1),
+                          _buildGameRow("Recently Added", recentlyAdded, 2),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25.0),
+                          child: Text(
+                            "v1.0.0",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 15),
+                        Image.asset(
+                          'assets/images/logo1.png', // Zamijeni sa svojim putem do slike
+                          height: 45,
+                          width: 45,
+                        ),
+                        const SizedBox(width: 15),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 25.0),
+                          child: StreamBuilder<DateTime>(
+                            stream: Stream.periodic(
+                              const Duration(seconds: 1),
+                              (_) => DateTime.now(),
+                            ),
+                            builder: (context, snapshot) {
+                              final currentTime =
+                                  snapshot.data ?? DateTime.now();
+                              final formattedTime =
+                                  "${currentTime.hour.toString().padLeft(2, '0')}:${currentTime.minute.toString().padLeft(2, '0')}:${currentTime.second.toString().padLeft(2, '0')}";
+                              return Text(
+                                formattedTime,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ),
-          ),
-         Container(
-  alignment: Alignment.bottomCenter,
-  width: double.infinity,
-  padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Padding(
-  padding: const EdgeInsets.only(left: 25.0), // Add left margin
-  child: Text(
-    "v1.0.0",
-    style: TextStyle(
-      color: Colors.white,
-      fontSize: 14,
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-),
-       const SizedBox(width: 15), 
-      Image.asset(
-        'assets/images/logo1.png', // Replace with your image path
-        height: 45, // Adjust the height of the image
-        width: 45,  // Adjust the width of the image
-        
-      ),
-     
-       const SizedBox(width: 15), // Add spacing between the image and text
-    Padding(
-  padding: const EdgeInsets.only(right: 25.0), // Add right margin
-  child: StreamBuilder<DateTime>(
-    stream: Stream.periodic(const Duration(seconds: 1), (_) => DateTime.now()),
-    builder: (context, snapshot) {
-      final currentTime = snapshot.data ?? DateTime.now();
-      final formattedTime = "${currentTime.hour.toString().padLeft(2, '0')}:${currentTime.minute.toString().padLeft(2, '0')}:${currentTime.second.toString().padLeft(2, '0')}";
-
-      return Text(
-        formattedTime,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 14,
-          fontWeight: FontWeight.bold,
+            );
+          },
         ),
-      );
-    },
-  ),
-),
-    ],
-  ),
-),
-        ],
       ),
-    ),
-  );
-}
+    );
+  }
 
   Widget _buildGameRow(String title, List<Game> games, int rowIndex) {
     return Column(
@@ -234,10 +312,12 @@ Widget build(BuildContext context) {
         Container(
           height: 350,
           child: ListView.builder(
+            controller: _horizontalScrollControllers[rowIndex],
             scrollDirection: Axis.horizontal,
             itemCount: games.length,
             itemBuilder: (context, index) {
-              bool isSelected = rowIndex == selectedRowIndex && index == selectedGameIndex;
+              bool isSelected =
+                  rowIndex == selectedRowIndex && index == selectedGameIndex;
               return Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: GameTile(
@@ -245,7 +325,7 @@ Widget build(BuildContext context) {
                   onGamePlayed: () {
                     _loadGames();
                   },
-                  isSelected: isSelected, 
+                  isSelected: isSelected,
                 ),
               );
             },
@@ -253,14 +333,5 @@ Widget build(BuildContext context) {
         ),
       ],
     );
-  }
-
-  void _startWatchingFolder() {
-    final path = Config.gamesDirectory;
-    _watcher = DirectoryWatcher(path);
-    _watcher!.events.listen((event) {
-      print('Detected change in folder: ${event.type} ${event.path}');
-      _loadGames();
-    });
   }
 }
--- lib/widgets/game_tile.dart
+++ lib/widgets/game_tile.dart
@@ -3,68 +3,76 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:arcade_os/models/game.dart';
 import 'package:arcade_os/services/game_service.dart';
+
 class GameTile extends StatelessWidget {
   final Game game;
   final VoidCallback? onGamePlayed;
   final bool isSelected; // Dodano za označavanje selektovane igre
 
-  const GameTile({super.key, required this.game, this.onGamePlayed, this.isSelected = false});
+  const GameTile({
+    super.key,
+    required this.game,
+    this.onGamePlayed,
+    this.isSelected = false,
+  });
 
   @override
-Widget build(BuildContext context) {
-  return SizedBox(
-    height: 400, // Set height
-    width: 200,  // Set width
-    child: GestureDetector(
-      onTap: () async {
-        onGamePlayed?.call();
-        GameService.updatePlayData(game.name);
-        String command = game.executablePath;
-        Process.start('cmd', ['/c', 'start', '', command])
-            .then((process) {
-              process.stdout.transform(utf8.decoder).listen((data) {
-                print('STDOUT: $data');
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 400, // Set height
+      width: 200, // Set width
+      child: GestureDetector(
+        onTap: () async {
+          onGamePlayed?.call();
+          GameService.updatePlayData(game.name);
+          String command = game.executablePath;
+          Process.start('cmd', ['/c', 'start', '', command])
+              .then((process) {
+                process.stdout.transform(utf8.decoder).listen((data) {
+                  print('STDOUT: $data');
+                });
+                process.stderr.transform(utf8.decoder).listen((data) {
+                  print('STDERR: $data');
+                });
+              })
+              .catchError((error) {
+                print('Error starting game: $error');
               });
-              process.stderr.transform(utf8.decoder).listen((data) {
-                print('STDERR: $data');
-              });
-            })
-            .catchError((error) {
-              print('Error starting game: $error');
-            });
-      },
-      child: Card(
-        color: isSelected ? Colors.blueGrey : Colors.transparent,
-        elevation: 0, // Oboji selektovanu igru
-        margin: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.file(
-                File(game.coverImagePath),
-                height: 300,
-                width: 200,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(height: 5),
-              Align(
-                alignment: Alignment.centerLeft, // Align text to the left
-                child: Text(
-                  game.name,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.white, // Ensure text color is white
-                    fontWeight: FontWeight.bold,
+        },
+        child: Card(
+          color:
+              isSelected
+                  ? const Color.fromARGB(255, 233, 183, 2)
+                  : Colors.transparent,
+          elevation: 0, // Oboji selektovanu igru
+          margin: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.file(
+                  File(game.coverImagePath),
+                  height: 300,
+                  width: 200,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 5),
+                Align(
+                  alignment: Alignment.centerLeft, // Align text to the left
+                  child: Text(
+                    game.name,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white, // Ensure text color is white
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              
-            ],
+              ],
+            ),
           ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
