e3bc87f7111981a381c243fc406d0b95238aff43
Amna Hodžić <ahodzic649@outlook.com>
2025-04-08 17:47:35 +0200

    selected game prettier

--- lib/widgets/game_tile.dart
+++ lib/widgets/game_tile.dart
@@ -27,18 +27,16 @@ class GameTile extends StatelessWidget {
           onGamePlayed?.call();
           GameService.updatePlayData(game.name);
           String command = game.executablePath;
-          Process.start('cmd', ['/c', 'start', '', command])
-              .then((process) {
-                process.stdout.transform(utf8.decoder).listen((data) {
-                  print('STDOUT: $data');
-                });
-                process.stderr.transform(utf8.decoder).listen((data) {
-                  print('STDERR: $data');
-                });
-              })
-              .catchError((error) {
-                print('Error starting game: $error');
-              });
+          Process.start('cmd', ['/c', 'start', '', command]).then((process) {
+            process.stdout.transform(utf8.decoder).listen((data) {
+              print('STDOUT: $data');
+            });
+            process.stderr.transform(utf8.decoder).listen((data) {
+              print('STDERR: $data');
+            });
+          }).catchError((error) {
+            print('Error starting game: $error');
+          });
         },
         child: Card(
           color: Colors.transparent,
@@ -62,21 +60,58 @@ class GameTile extends StatelessWidget {
                       width: size,
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color:
-                              isSelected
-                                  ? const Color.fromARGB(186, 233, 183, 2)
-                                  : Colors.transparent,
+                          color: isSelected
+                              ? const Color.fromARGB(186, 233, 183, 2)
+                              : Colors.transparent,
                           width: isSelected ? 2 : 0,
                         ),
                         borderRadius: BorderRadius.circular(0),
                       ),
-                      child: AnimatedOpacity(
-                        duration: const Duration(milliseconds: 200),
-                        opacity: isSelected ? 1.0 : 0.8,
-                        child: Image.file(
-                          File(game.coverImagePath),
-                          fit: BoxFit.cover,
-                        ),
+                      child: Stack(
+                        children: [
+                          Image.file(
+                            File(game.coverImagePath),
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: double.infinity,
+                          ),
+                          if (isSelected)
+                            TweenAnimationBuilder<double>(
+                              tween: Tween<double>(begin: -1.0, end: 2.0),
+                              duration: const Duration(seconds: 2),
+                              curve: Curves.easeInOut,
+                              builder: (context, value, child) {
+                                final double opacity = (value < 1.5)
+                                    ? 1.0 - (value - 1.0).clamp(0.0, 0.5) * 2
+                                    : 0.0;
+
+                                return Opacity(
+                                  opacity: opacity,
+                                  child: ShaderMask(
+                                    shaderCallback: (rect) {
+                                      return LinearGradient(
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomRight,
+                                        colors: [
+                                          Colors.white.withOpacity(0.3),
+                                          Colors.transparent,
+                                        ],
+                                        stops: [value, value + 0.3],
+                                      ).createShader(rect);
+                                    },
+                                    blendMode: BlendMode.srcATop,
+                                    child: child,
+                                  ),
+                                );
+                              },
+                              child: Image.file(
+                                File(game.coverImagePath),
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                                height: double.infinity,
+                              ),
+                            ),
+                        ],
                       ),
                     );
                   },
@@ -85,21 +120,20 @@ class GameTile extends StatelessWidget {
                 AnimatedOpacity(
                   opacity: isSelected ? 1.0 : 0.0,
                   duration: const Duration(milliseconds: 200),
-                  child:
-                      isSelected
-                          ? Align(
-                            alignment: Alignment.centerLeft,
-                            child: MarqueeText(
-                              text: game.name,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              containerWidth: 200,
+                  child: isSelected
+                      ? Align(
+                          alignment: Alignment.centerLeft,
+                          child: MarqueeText(
+                            text: game.name,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
                             ),
-                          )
-                          : const SizedBox.shrink(),
+                            containerWidth: 200,
+                          ),
+                        )
+                      : const SizedBox.shrink(),
                 ),
               ],
             ),
